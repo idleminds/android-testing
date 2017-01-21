49fa307238e7495a29075fa77d420024b785c342
Gary Moyer <gmoyer@ebay.com>
2017-01-21 15:45:48 -0800

    Rename to avoid clash with the Test annotation; clean up unused imports

--- app/src/androidTest/java/com/example/android/testing/notes/custom/TestNotesActivity.java
+++ app/src/androidTest/java/com/example/android/testing/notes/custom/TestNotesActivity.java
@@ -3,28 +3,21 @@ package com.example.android.testing.notes.custom;
 import android.content.Context;
 import android.content.Intent;
 import android.support.test.InstrumentationRegistry;
-import android.support.test.espresso.ViewInteraction;
 import android.support.test.runner.AndroidJUnit4;
 
-import com.example.android.testing.notes.R;
 import com.example.android.testing.notes.notes.NotesActivity;
 
+import org.junit.Test;
 import org.junit.runner.RunWith;
 
-import static android.support.test.espresso.Espresso.onView;
-import static android.support.test.espresso.assertion.ViewAssertions.matches;
-import static android.support.test.espresso.matcher.ViewMatchers.isClickable;
-import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
-import static android.support.test.espresso.matcher.ViewMatchers.withId;
 
 @RunWith(AndroidJUnit4.class)
-public class Test
+public class TestNotesActivity
 {
-    @org.junit.Test
+    @Test
     public void test()
     {
         Context context = InstrumentationRegistry.getTargetContext();
-
         Intent intent = new Intent(context, NotesActivity.class);
         intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
         context.startActivity(intent);
